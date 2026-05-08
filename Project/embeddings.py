b89dc399e56cbd561379fff0865924de6a1afec6
priyanshupandey-ml <mllabpriyanshu@gmail.com>
2026-05-08 11:35:22 +0530

    Update embeddings.py

--- Project/embeddings.py
+++ Project/embeddings.py
@@ -1,6 +1,7 @@
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from dotenv import load_dotenv
 import os   
+import streamlit as st
 load_dotenv()
 
 
@@ -9,6 +10,6 @@ def get_embeddings():
     embeddings = HuggingFaceEndpointEmbeddings(
         model="BAAI/bge-base-en-v1.5",
         task="feature-extraction",
-        huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+        huggingfacehub_api_token=st.secrets["HUGGINGFACEHUB_API_TOKEN"],
     )
     return embeddings
